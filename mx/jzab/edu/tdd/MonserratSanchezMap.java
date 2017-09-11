a168bda3b20f60fb4924146c1d7f1b8c9c34b00f
Monserrat Genereux <monserratgenereux@gmail.com>
2017-09-10 19:45:37 -0500

    fix

--- mx/jzab/edu/tdd/MonserratSanchezMap.java
+++ mx/jzab/edu/tdd/MonserratSanchezMap.java
@@ -5,93 +5,93 @@ import java.util.Collection;
 import java.util.NoSuchElementException;
 
 public class MonserratSanchezMap<K, V> implements IMap<K, V>{
-	
-	private int size;
-	private Object[] arr = new Object[25];
-	
-	 
-	private int hash(Object key){
-		return Math.abs(key.hashCode()%this.arr.length);
-	}
-	
-	
-	@Override
-	public void clear() {
-		this.arr = new Object[103];
-		this.size = 0;
-		
-	}
-
-	@Override
-	public boolean containsKey(Object key) {
-		if(arr[hash(key)]!=null)
-			return true;
-		return false;
-	}
-
-	@Override
-	public boolean containsValue(Object value) {
-		for(Object obj : this.arr)
-			if(obj != null && obj.equals(value))	
-				return true;
-		return false;
-	}
-
-	@Override
-	public Object get(Object key) {
-		return getOrDefault(key, " ");
-	}
-
-	@Override
-	public Object getOrDefault(Object key, Object defaultValue){
-		Object Item = this.arr[hash(key)];
-		if(Item == null)
-			return defaultValue;
-		return Item;
-	}
-	
-
-	@Override
-	public boolean isEmpty() {
-		if(this.size == 0)
-			return true;
-		return false;
-	}
-
-	@Override
-	public void put(Object key, Object value) {
-		this.arr[hash(key)] = value;
-		this.size++;
-		
-	}
-
-	@Override
-	public Object remove(Object key) {
-		if(this.containsKey(key)){
-			Object Item = this.arr[hash(key)];
-			this.arr[hash(key)] = null;
-			this.size--;
-			return Item;
-		}
-			
-		return null;
-	}
-
-	@Override
-	public void replace(Object key, Object value) throws NoSuchElementException {
-		if(!this.containsKey(key))
-			throw new NoSuchElementException();
-		this.arr[hash(key)] = value;
-	}
-
-	@Override
-	public int size() {
-		return this.size;
-	}
-
-	@Override
-	public Collection values() {	
-		return Arrays.asList(arr);
-	}
-
+    
+    private int size;
+    private Object[] arr = new Object[25];
+    
+    
+    private int hash(Object key){
+        return Math.abs(key.hashCode()%this.arr.length);
+    }
+    
+    
+    @Override
+    public void clear() {
+        this.arr = new Object[25];
+        this.size = 0;
+        
+    }
+    
+    @Override
+    public boolean containsKey(Object key) {
+        if(arr[hash(key)]!=null)
+            return true;
+        return false;
+    }
+    
+    @Override
+    public boolean containsValue(Object value) {
+        for(Object obj : this.arr)
+            if(obj != null && obj.equals(value) )
+                return true;
+        return false;
+    }
+    
+    @Override
+    public Object get(Object key) {
+        return getOrDefault(key, null);
+    }
+    
+    @Override
+    public Object getOrDefault(Object key, Object defaultValue){
+        Object Item = this.arr[hash(key)];
+        if(Item == null)
+            return defaultValue;
+        return Item;
+    }
+    
+    
+    @Override
+    public boolean isEmpty() {
+        if(this.size == 0)
+            return true;
+        return false;
+    }
+    
+    @Override
+    public void put(Object key, Object value) {
+        this.arr[hash(key)] = value;
+        this.size++;
+        
+    }
+    
+    @Override
+    public Object remove(Object key) {
+        if(this.containsKey(key)){
+            Object Item = this.arr[hash(key)];
+            this.arr[hash(key)] = null;
+            this.size--;
+            return Item;
+        }
+        
+        return null;
+    }
+    
+    @Override
+    public void replace(Object key, Object value) throws NoSuchElementException {
+        if(!this.containsKey(key))
+            throw new NoSuchElementException();
+        this.arr[hash(key)] = value;
+    }
+    
+    @Override
+    public int size() {
+        return this.size;
+    }
+    
+    @Override
+    public Collection values() {	
+        return Arrays.asList(arr);
+    }
+    
 }
